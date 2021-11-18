ef50aa797bd15dfb589492342266c42b3b81b1ac
antolam49 <anto.lam49@gmail.com>
2021-11-18 22:13:51 +0100

    Start Console + Personne_Metier

--- ProjetSymfoCS/ProjetSymfoCS.Metier/Personne_Metier.cs
+++ ProjetSymfoCS/ProjetSymfoCS.Metier/Personne_Metier.cs
@@ -1,7 +1,15 @@
-﻿namespace ProjetSymfoCS.Metier
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ProjetSymfoCS.Metier
 {
     public class Personne_Metier
     {
-
+        
     }
 }
